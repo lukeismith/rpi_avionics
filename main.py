dc85fac43a5d37e69df4521c27137fe9f5a7bf0c
Luke <lukeis2@illinois.edu>
2018-06-27 02:00:10 +0000

    bno stuff started, vim sucks

--- main.py
+++ main.py
@@ -7,32 +7,44 @@ import time
 import threading
 import Adafruit_ADS1x15
 import gpiozero
+import logging
+import sys
+
 from picamera import PiCamera
 from libs.accelerometerThread import accelerometerThread
 from libs.cameraThread import cameraThread
+from Adafruit_BNO055 import BNO055
 
 def main():
     # Initialize adc module
-    adc = Adafruit_ADS1x15.ADS1115()
+    #adc = Adafruit_ADS1x15.ADS1115()
+
     # Keep recording until this is set to true
     record_data = threading.Event()
     # Initialize gpio pins
-    status_led = gpiozero.LED(23)
-    record_switch = gpiozero.Button(24)
+    #status_led = gpiozero.LED(23)
+    #record_switch = gpiozero.Button(24)
 
     # Initialize SPI device
     # 
-    sense_pressure = gpiozero.SPIDevice()
+    #sense_pressure = gpiozero.SPIDevice()
 
-    xyzs = accelerometerThread(adc, record_data)
-    cam = cameraThread(record_data)
+    #xyzs = accelerometerThread(adc, record_data)
+    #cam = cameraThread(record_data)
 
-    xyzs.start()
-    cam.start()
+    bno = BNO055.BNO055(serial_port='/dev/serial10', rst=18)
+
+    if len(sys.argv) == 2 and sys.argv[1].lower() == '-v':
+        logging.basicConfig(level=logging.DEBUG)
+
+    
+
+    #xyzs.start()
+    #cam.start()
 
     while True:
 
-        if not record_switch.is_pressed:
+        if record_switch.is_pressed:
             record_data.set() # Sets event to true to signal threads to stop
 
         if not record_data:
